0c60c3c499b837dbc763701ea7af4c9012fbf77f
Donald-cmd-ops <donaldwatchesracing@gmail.com>
2024-12-19 22:17:50 +1100

    first push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import * as FaIcons from "react-icons/fa"
+import * as AiIcons from "react-icons/ai"
+import { SidebarData } from './SidebarData'
+import './Navbar.css'
+import { IconContext } from 'react-icons'
+import TrainingTransparent from '../assets/Training_transparent.png';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false)
+
+  const showSidebar = () => setSidebar(!sidebar)
+
+  return (
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+      </head>
+      <IconContext.Provider value={{ color: 'purple' }}>
+        <div className='navbar'>
+          <div className='navbar-left'>
+            <Link to="#" className='menu-bars'>
+              <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+          </div>
+          <h1 className='montserrat-alternates-regular'>Training Sphere</h1>
+          <div className='navbar-right'>
+            <Link to="Signup" className='login'>
+              <a>Sign Up</a>
+            </Link>
+            <Link to="Login" className='signup'>
+              <a>Login</a>
+            </Link>
+          </div>
+
+        </div>
+
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar} >
+            <li className="navbar-toggle">
+              <Link to="#" className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  )
+}
+
+export default Navbar
+
