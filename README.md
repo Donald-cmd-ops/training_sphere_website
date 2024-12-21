2456ddb8773e9d564eb19963109e32a1d369d1d1
Donald-cmd-ops <donaldwatchesracing@gmail.com>
2024-12-21 20:36:05 +1100

    Changes made to post form

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Following resources have been referenced in the development of this website :
 https://v1.tailwindcss.com/components/buttons
 https://v1.tailwindcss.com/components/cards
 https://www.youtube.com/watch?v=CXa0f4-dWi4&t=224s
+select bar : https://www.material-tailwind.com/docs/html/select
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,9 @@
 import './css/post_styles.css'
 function Post() {
+    const postThread = async () => {
+
+    }
+
     return (
         <>
             <body>
@@ -8,14 +12,22 @@ function Post() {
                         <form action="/submit-workout" method="post">
                             <h1>Training Program Description</h1>
                             <label htmlFor="sport">Target Sport</label>
-                            <select id="sport" name="sport" required>
-                                <option value="">Select a sport</option>
-                                <option value="running">Running</option>
-                                <option value="swimming">Swimming</option>
-                                <option value="football">Football</option>
-                                <option value="weightlifting">Weightlifting</option>
-                                <option value="other">Other</option>
-                            </select>
+                            <div class="w-full max-w-sm min-w-[200px]">
+                                <div class="relative">
+                                    <select
+                                        className="w-full placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                                        <option value="swimming">Swimming</option>
+                                        <option value="wresting">Wrestling</option>
+                                        <option value="boxing">Boxing</option>
+                                        <option value="bjj">Brazillian jujitsu</option>
+                                        <option value="running">Running</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                    </svg>
+                                </div>
+                            </div>
 
                             <label htmlFor="title">Training Program Name</label>
                             <input
@@ -51,7 +63,7 @@ function Post() {
                                 placeholder="Document challenges or self-feedback"
                             ></textarea>
 
-                            <button type="submit">Post Workout</button>
+                            <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Post Training</button>
                         </form>
                     </div>
                     <div className='container2'>
